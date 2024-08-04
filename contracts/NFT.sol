d164036ec0bfd0e4f60f13206cd94e54417d9835
azmathn88 <azmathsn88@gmail.com>
2024-08-04 22:00:09 +0000

    feat: initiated the project

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("pop","po")
+        ERC721("poy","py")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
